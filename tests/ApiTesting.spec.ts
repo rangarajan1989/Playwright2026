49e2453c2b62a1c537d0742b8016e04fd7db03e7
= <rangarajan.ragavan@gmail.com>
2026-06-29 10:24:21 +0530

    Adding jenkis file

---
+++ tests/ApiTesting.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect, chromium } from "@playwright/test";
+const TOKEN =
+  "22a2c4a87d8c9d4466400a6503338f43b5868f2114fca90f356b4c21280d2047";
+
+test("get request", async ({ request }) => {
+  const requestBody = await request.get(
+    "https://gorest.co.in/public/v2/users",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    },
+  );
+  expect(requestBody.status()).toBe(200);
+  const data = await requestBody.json();
+  console.log(data);
+});
+
+test("post request", async ({ request }) => {
+  const requestData = {
+    name: "Raghu R",
+    email: `raghu${Date.now()}@gmail.com`,
+    gender: "female",
+    status: "active",
+  };
+  const requestBody = await request.post(
+    "https://gorest.co.in/public/v2/users",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      data: requestData,
+    },
+  );
+  expect(requestBody.status()).toBe(201);
+  const data = await requestBody.json();
+  console.log(data);
+});
+
+test("put request", async ({ request }) => {
+  const userId = "8522791";
+  const requestData = {
+    status: "active",
+  };
+  const requestBody = await request.put(
+    `https://gorest.co.in/public/v2/users/${userId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      data: requestData,
+    },
+  );
+  expect(requestBody.status()).toBe(200);
+  const data = await requestBody.json();
+  console.log(data);
+});
--- tests/SelectDropDown1.ts
+++ tests/SelectDropDown1.ts
@@ -4,4 +4,6 @@ import { Browser, chromium, expect, Locator } from "@playwright/test";
   let browser = await chromium.launch({ headless: false, channel: "chrome" });
   let page = await browser.newPage();
   await page.goto("https://orangehrm.com/contact-sales");
+  let selectLocator = page.locator(`select#Form_getForm_Country`);
+  await selectLocator.selectOption("Algeria");
 })();
